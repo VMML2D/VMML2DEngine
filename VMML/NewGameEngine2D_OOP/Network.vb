17ce18e8cd811785f15fe80299e9a8fe27f936fa
VMML2D <cprogrammingit@hotmail.com>
2013-09-08 14:19:15 +0200

    Network-namespace added

---
+++ VMML/NewGameEngine2D_OOP/Network.vb
@@ -0,0 +1,29 @@
+﻿Namespace NetworkSettingUp
+    Public Class NetworkStatus
+
+    End Class
+    Public Class Connect
+
+    End Class
+    Public Class LogIn
+
+    End Class
+    Public Class Register
+
+    End Class
+    Public Class RunServer
+
+    End Class
+End Namespace
+
+Namespace SendData
+    Public Class AnalyseServer
+
+    End Class
+    Public Class Migrate
+
+    End Class
+    Public Class SendData
+
+    End Class
+End Namespace
